e2d2eaed4c98d1a6192dee20e5ca51aadb0744d4
kimtree <iam@kimtree.net>
2018-03-26 17:21:49 +0900

    20180326

---
+++ laboratory/20180326.py
@@ -0,0 +1,38 @@
+import collections
+import random
+
+a = 1
+b = 'a'
+c = True
+
+def print_dd():
+	print('dddddddd')
+
+def get_player_name(name):
+	b = 'blah'
+	return b + ' player ' + name
+
+def sum(number1, number2=10, number3=1):
+	return number1 + number2 + number3
+
+print('dljlkjlkdjf', end='')
+
+
+def mult(number1, number2):
+	return number1 * number2
+
+def isEven(number):
+	return number % 2 == 0
+
+if isEven(4):
+	print('4 is even')
+else:
+	print('4 is odd')
+
+# 숫자와 문자 비교
+cave = 1
+cave_str = '1'
+#print(cave == cave_str)
+#print(type(cave))
+#print(type(cave_str))
+#print(type(True))
